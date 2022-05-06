3a8c07730e2e675aa3859c6241b935acc8dd418e
Yewon Kim <victory2588@naver.com>
2022-05-07 02:57:55 +0900

    선미거 가져옴...

--- app/src/main/java/com/java/swuchunky/MainActivity.java
+++ app/src/main/java/com/java/swuchunky/MainActivity.java
@@ -4,16 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.app.Activity;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.java.swuchunky.wizard.main_calendar.wizard_main;
-import com.java.swuchunky.wizard.wizard_mypage;
-import com.java.swuchunky.wizard.reserve.wizard_reserve;
-import com.java.swuchunky.wizard.reserveview.wizard_reserveview;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,41 +25,39 @@ public class MainActivity extends AppCompatActivity {
         //하단 네비게이션 선택시 반응 리스너 설정
         bottom_menu.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
 
-        //처음 보일 프래그먼트
-        wizard_main = (wizard_main) getSupportFragmentManager().findFragmentById(R.id.nav_wizard_main);
-
         //프래그먼트 객체 생성
         wizard_main = new wizard_main();
         wizard_mypage = new wizard_mypage();
         wizard_reserve = new wizard_reserve();
         wizard_reserveview = new wizard_reserveview();
+
+        //처음 보일 프래그먼트
+        getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, wizard_main).commit();
     }
 
 
-    public boolean onNavigationItemSelected (MenuItem item) {
+    public boolean onNavigationItemSelected (@NonNull MenuItem item) {
         int id = item.getItemId();
-
         //홈버튼 선택
-        if (id == R.id.nav_wizard_main) {
+        if (id == R.id.wizard_main) {
             getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, wizard_main).commit();
             return true;
         }
         //예약리스트 보기 선택
-        else if (id == R.id.nav_wizard_reserveview) {
+        if (id == R.id.wizard_reserveview) {
             getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, wizard_reserveview).commit();
             return true;
         }
         //검색(예약하기) 선택
-        else if (id == R.id.nav_wizard_reserve) { //id == R.id.wizard_reserve
+        if (id == R.id.wizard_reserve) {
             getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, wizard_reserve).commit();
             return true;
         }
         //마이페이지 선택
-        else if (id == R.id.nav_wizard_mypage) {
+        if (id == R.id.wizard_mypage) {
             getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, wizard_mypage).commit();
             return true;
         }
         return true;
     }
-
 }
