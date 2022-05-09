fcd4cbaffbe3c3bcfca6e5ed5fa9a861231a7ef8
Yewon Kim <victory2588@naver.com>
2022-05-10 00:33:00 +0900

    도비 로그인

--- app/src/main/java/com/java/swuchunky/dobi_login.java
+++ app/src/main/java/com/java/swuchunky/dobi_login.java
@@ -1,15 +1,76 @@
 package com.java.swuchunky;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.app.Fragment;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+//import androidx.fragment.app.Fragment;
 
-import com.java.swuchunky.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class dobi_login extends AppCompatActivity {
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    Button mLoginBtn;
+    TextView wizard_membership_txt;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dobi_login);
+        setContentView(R.layout.wizard_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        //버튼 등록하기
+        wizard_membership_txt = findViewById(R.id.dobi_membership_txt);
+        mLoginBtn = findViewById(R.id.dobi_login_btn);
+        mEmailText = findViewById(R.id.dobi_email_Insert);
+        mPasswordText = findViewById(R.id.dobi_pw_Insert);
+
+
+        //가입 버튼이 눌리면
+        wizard_membership_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(getApplicationContext(), dobi_signup.class));
+            }
+        });
+
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(dobi_login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(dobi_login.this, wizardMainActivity.class); //getApplicationContext()
+                                    startActivity(intent);
+                                    Toast.makeText(getApplicationContext(),"환영합니다!",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(getApplicationContext(),"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
