e144cae00f4b05c9c9d3f3ce5a70881903da671c
Yewon Kim <victory2588@naver.com>
2022-06-06 18:41:50 +0900

    0606 청소 지원하기 구현중

--- app/src/main/java/com/java/swuchunky/dobiMainActivity.java
+++ app/src/main/java/com/java/swuchunky/dobiMainActivity.java
@@ -14,7 +14,7 @@ public class dobiMainActivity extends FragmentActivity {
     dobi_main dmain;
 
     private BottomNavigationView dobi_menu; //하단 네비게이션
-    private Fragment dobi_main, dobi_mypage, dobi_apply, dobi_findWork1; //네비게이션을 통해 이동할 프래그먼트
+    private Fragment dobi_main, dobi_mypage, dobi_ceritified, dobi_findWork1; //네비게이션을 통해 이동할 프래그먼트
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class dobiMainActivity extends FragmentActivity {
 
         dobi_main = new dobi_main();
         dobi_mypage = new dobi_mypage();
-        dobi_apply = new dobi_apply();
+        dobi_ceritified = new dobi_ceritified();
         dobi_findWork1 = new dobi_findWork1();
 
 
@@ -49,7 +49,7 @@ public class dobiMainActivity extends FragmentActivity {
                 }
                 //예약리스트 보기 선택
                 if (id == R.id.nav_dobi_apply) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, dobi_apply).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.menu_containers, dobi_ceritified).commit();
                     return true;
                 }
                 //검색(예약하기) 선택
---
+++ app/src/main/java/com/java/swuchunky/dobi_ceritified_adapter.java
@@ -0,0 +1,65 @@
+package com.java.swuchunky;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class dobi_ceritified_adapter extends RecyclerView.Adapter<dobi_ceritified_adapter.CustomViewHolder> {
+    private ArrayList<reservation_info> arrayList;
+    private Context context;
+
+    public dobi_ceritified_adapter(ArrayList<reservation_info> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.dobi_ceritified_item,parent,false);
+        CustomViewHolder holder=new CustomViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        holder.email.setText(arrayList.get(position).getEmail());
+        holder.category.setText(arrayList.get(position).getCategory());
+        holder.date.setText(String.valueOf(arrayList.get(position).getWhen()));
+        holder.request.setText(arrayList.get(position).getRequest());
+        holder.where.setText(arrayList.get(position).getWhere());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList != null ? arrayList.size():0;
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView category;
+        TextView date;
+        TextView request;
+        TextView where;
+        TextView email;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView=itemView.findViewById(R.id.wizard_image);
+            this.email=itemView.findViewById(R.id.wizard_name);
+            this.category=itemView.findViewById(R.id.clean_category1);
+            this.date=itemView.findViewById(R.id.clean_date1);
+            this.request=itemView.findViewById(R.id.clean_request1);
+            this.where=itemView.findViewById(R.id.clean_where1);
+        }
+    }
+}
